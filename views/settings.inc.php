695edac1300da5ba0a65bc385159b38390804e39
Jordan Layman <jordanmlayman@gmail.com>
2024-01-07 17:45:44 -0800

    settings page working and added MM api

--- views/settings.inc.php
+++ views/settings.inc.php
@@ -9,27 +9,54 @@ if (isset($_POST['submit'])) {
 	
 	} else {
 
-		$update_token_reader_count = $_POST['token_reader_count'];  
-		update_option('iflpm_token_reader_count', $update_token_reader_count);
+		pr("Settings Submitted Successfully");
+		pr($_POST);
+
+		$update_iflzpo_token_reader_count = $_POST['iflzpo_token_reader_count'];  
+		update_option('iflzpo_token_reader_count', $update_iflzpo_token_reader_count);
+
+		$update_iflzpo_mm_api_key = sanitize_text_field($_POST['iflzpo_mm_api_key']);  
+		update_option('iflzpo_mm_api_key', $update_iflzpo_mm_api_key);
 		
-		self::log_action("IFLPM Settings Updated Successfully");
+		$update_iflzpo_mm_api_secret = sanitize_text_field($_POST['iflzpo_mm_api_secret']);  
+		update_option('iflzpo_mm_api_secret', $update_iflzpo_mm_api_secret);
+
+		self::log_action("IFLZPO Settings Updated Successfully");
 	}
 }	
 
-$token_reader_count = get_option('iflpm_token_reader_count');
 
+$iflzpo_mm_api_key = get_option('iflzpo_mm_api_key');
+$iflzpo_mm_api_secret = get_option('iflzpo_mm_api_secret');
+
+$iflzpo_token_reader_count = get_option('ifzpo_token_reader_count');
 ?>
 
 <?php include 'admin-header.inc.php'  ?>
 
-<form name="iflpm_settings" method="post" action="#">
-	<?php wp_nonce_field('settings-submit','settings-submit-nonce'); ?>
+<h2>MemberMouse Settings</h2>
+<?php if (isset($errors)) { ?>
+	<div class="notice notice-error is-dismissible">
+		<p><?php echo $errors['nonce-error']; ?></p>
+	</div>
+<?php } ?>
 
+<form name="iflzpo_settings" method="post" action="#">
+	<?php wp_nonce_field('settings-submit','settings-submit-nonce'); ?>
+	
 	<table class="form-table" role="presentation">
 		<tbody>
 			<tr>
-				<th><label for="token_reader_count">Token Reader Count</label></th>
-				<td><input name="token_reader_count" id="token_reader_count" value="<?php echo $token_reader_count; ?>" type="text" class="regular-text" /></td>
+				<th><label for="iflzpo_mm_api_key">MemberMouse API Key</label></th>
+				<td><input name="iflzpo_mm_api_key" id="iflzpo_mm_api_key" value="<?php echo $iflzpo_mm_api_key; ?>" type="text" class="regular-text" /></td>
+			</tr>
+			<tr>
+				<th><label for="iflzpo_mm_api_secret">MemberMouse API Secret</label></th>
+				<td><input name="iflzpo_mm_api_secret" id="iflzpo_mm_api_secret" value="<?php echo $iflzpo_mm_api_secret; ?>" type="password" class="regular-text" /></td>
+			</tr>
+			<tr>
+				<th><label for="iflzpo_token_reader_count">Token Reader Count</label></th>
+				<td><input name="iflzpo_token_reader_count" id="iflzpo_token_reader_count" value="<?php echo $iflzpo_token_reader_count; ?>" type="text" class="regular-text" /></td>
 			</tr>
 		</tbody>	
 	</table>
